51fbb9f9d931978be35cdceae2b8f52d90736c17
Yuchen1008611 <875871898@qq.com>
2019-08-24 09:35:22 +0800

    0824

--- src/pages/home/home/Home.js
+++ src/pages/home/home/Home.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
-
-class Home extends Component {
-    render() {
-        return (
-            <div>
-                首页
-            </div>
-        );
-    }
+import React from 'react';
+import AppScroll from '../../../components/home/app-scroll'
+import AppHeader from '../../../components/app-header/AppHeader'
+import Banner from '../home/home/banner'
+class Home extends React.Component{
+    render(){
+    return(
+       <div className="pege" id="home">
+            <AppHeader></AppHeader>
+            <AppScroll className="content">
+            <Banner></Banner> 
+           </AppScroll>
+           
+       </div> 
+    )
+}
 }
 
 export default Home;
---
+++ src/pages/home/home/home/banner.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+
+class banner extends Component {
+    swiperDOM = React.createRef();
+    render() {
+        const listData = [
+            {id: 1, url: '/static/images/home/banner1.jpg'},
+            {id: 2, url: '/static/images/home/banner2.jpg'},
+            {id: 3, url: '/static/images/home/banner3.jpg'},
+            {id: 4, url: '/static/images/home/banner4.jpg'},
+            
+        ]
+        return (
+            <div className="swiper-container" ref={this.swiperDOM}>
+            <div className="swiper-wrapper">
+            {
+          listData.map((item)=>(
+            <div className="swiper-slide" key={item.id}>
+              <img src={item.url} alt=""/>
+              <h1>{item.id}</h1>
+              <h2>text</h2>
+              <h2>text</h2>
+              <h2>text</h2>
+              <h2>text</h2>
+              <h2>text</h2>
+              <h2>text</h2>
+              <h2>text</h2>
+              <h2>text</h2>
+              <h2>text</h2>
+              <h2>text</h2>
+            </div>
+          ))
+        }      
+            </div>
+           
+            <div className="swiper-pagination"></div>
+        </div>
+        
+        )
+    }
+    // componentDidMount(){
+    //     // 初始化轮播图
+    //     this.swiper = new window.Swiper(this.swiperDOM.current, {
+    //       pagination: '.swiper-pagination',
+    //        loop: true
+    //     });
+    //   }
+    //   componentDidUpdate(){
+    //      // 数据发生变化，先销毁，再重新创建轮播图
+    //      this.swiper.destroy();
+    //      this.swiper = new window.Swiper(this.swiperDOM.current, {
+    //        pagination: '.swiper-pagination',
+    //       loop: true
+    //      });
+    //    }
+}
+
+export default banner;
