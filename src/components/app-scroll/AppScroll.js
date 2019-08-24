e4c8e8062a55018eeb3a22ecbf2f3a89557de5b4
herman-na <1140128129@qq.com>
2019-08-24 09:12:00 +0800

    三个公共组件-增加better-scroll依赖-增加border-css文件

--- src/components/app-scroll/AppScroll.js
+++ src/components/app-scroll/AppScroll.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import BScroll from 'better-scroll'
+import './style.scss'
 
-class AppScroll extends Component {
+export default class AppScroll extends Component {
+    scrollDOM = React.createRef()
     render() {
+        let {className,children} = this.props
         return (
-            <div>
-                滚动组件
+            <div className={`scroll-wrap ${className}`} ref={this.scroll}>
+                <div className='scroll-content'>
+                    {children}
+                </div>
             </div>
         );
     }
+    componentDidMount(){
+        this.scroll = new BScroll(this.scrollDOM.current,{
+            tap:true,
+            click:true,
+            scrollX:false,
+            scrollY:true
+        });
+        this.scroll.on('beforeScrollStart',()=>{
+            this.scroll.refresh()
+        })
+    }
+    scrollTo = (y)=>{
+        let maxY = this.scroll.maxScrollY;
+        y >= maxY ? this.scroll.scrollTo(0, y, 200): this.scroll.scrollTo(0, maxY, 200);
+    }
 }
 
-export default AppScroll;
